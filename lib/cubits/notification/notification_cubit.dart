74a2f0d118469bee4d77bb46f483e5588ce1cc15
Imad-Belatreche <imad.belatreche@etu.usthb.dz>
2026-07-19 05:17:58 +0100

    Closes #19: Compute nextTrigger on each addNotification and refresh os level on every add, delete, update of notification
    - Move ensureNotificationPermission from notification_service to here.
    - Fix import paths
    - Handle some exceptions

--- lib/cubits/notification/notification_cubit.dart
+++ lib/cubits/notification/notification_cubit.dart
@@ -1,17 +1,19 @@
 import 'dart:developer';
 
-import 'package:dakerni/models/notification_model.dart';
-import 'package:dakerni/repositories/notifications_reposiory.dart';
-import 'package:dakerni/services/notificatio_service.dart';
+import 'package:dakerni/models/notification_rule_model.dart';
+import 'package:dakerni/repositories/notifications_repository.dart';
+import 'package:dakerni/services/notification_service.dart';
 import 'package:dakerni/utils/exceptions.dart';
-import 'package:dakerni/utils/general_utils.dart';
+import 'package:dakerni/utils/recurrence_calculator.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 part 'notification_state.dart';
 
 class NotificationCubit extends Cubit<NotificationState> {
   NotificationCubit() : super(NotificationState(notifications: []));
-  final _notificationRepository = NotificationsReposiory();
+  final _notificationRepository = NotificationsRepository();
   final _notificationService = NotificationService.instance;
 
   Future<void> loadNotifications() async {
@@ -28,14 +30,69 @@ class NotificationCubit extends Cubit<NotificationState> {
     }
   }
 
-  Future<NotificationModel> addNotification(
-    NotificationModel notification,
+  // inside e.g. NotificationSettingsCubit
+  Future<void> ensureNotificationPermission(BuildContext context) async {
+    final status = await NotificationService.instance
+        .checkNotificationPermissionStatus();
+
+    if (status.isPermanentlyDenied || status.isDenied) {
+      if (!context.mounted) return;
+      final shouldOpenSettings = await showDialog<bool>(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: const Text('Notifications Permission Required'),
+          content: const Text(
+            'Please allow notifications permission in settings to receive reminders.',
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context, false),
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () => Navigator.pop(context, true),
+              child: const Text('Open Settings'),
+            ),
+          ],
+        ),
+      );
+      if (shouldOpenSettings == true) await openAppSettings();
+    } else if (!status.isGranted) {
+      await NotificationService.instance.requestExactAlarmPermission();
+    }
+  }
+
+  Future<NotificationRuleModel> addNotification(
+    NotificationRuleModel notification,
   ) async {
     int? insertedId;
+
     try {
       emit(state.copyWith(isLoading: true, errorMessage: null));
+
+      final nextTrigger = RecurrenceCalculator.computeNextTrigger(notification);
+      if (nextTrigger == null) {
+        throw NotificationException(
+          "Failed to compute next trigger for notification: $notification",
+        );
+      } else if (nextTrigger.error != null) {
+        throw NotificationException(
+          "Error computing next trigger for notification: $notification. Error: ${nextTrigger.error}",
+        );
+      }
+
+      notification = notification.copyWith(
+        nextTriggerAt: Optional(nextTrigger.nextTrigger),
+      );
+
       insertedId = await _notificationRepository.addNotification(notification);
+      if (insertedId == -1) {
+        throw NotificationException(
+          "Failed to add notification to the database: $notification",
+        );
+      }
       log("Added notification id: $insertedId");
+
       final addedNotification = await _notificationRepository
           .getNotificationById(insertedId);
 
@@ -45,15 +102,7 @@ class NotificationCubit extends Cubit<NotificationState> {
         );
       }
 
-      await _notificationService.scheduleReminder(
-        id: addedNotification.id,
-        title: "Remember",
-        body: addedNotification.content.isEmpty
-            ? "You didn't type anything, but I will remind you anyway."
-            : addedNotification.content,
-        scheduledDate: convertToTZDateTime(addedNotification.scheduledDate),
-      );
-
+      await _notificationService.refreshScheduledNotifications();
       await loadNotifications();
       return addedNotification;
     } on NotificationException catch (e) {
@@ -61,6 +110,7 @@ class NotificationCubit extends Cubit<NotificationState> {
         await _notificationRepository.deleteNotification(insertedId);
       }
       emit(state.copyWith(errorMessage: e.toString()));
+
       rethrow;
     } catch (e) {
       emit(state.copyWith(errorMessage: e.toString()));
@@ -68,25 +118,34 @@ class NotificationCubit extends Cubit<NotificationState> {
     } finally {
       if (state.isLoading) {
         emit(state.copyWith(isLoading: false));
+        log(
+          "NotificationCubit: Finished addNotification process. Error: ${state.errorMessage}",
+        );
       }
     }
   }
 
-  Future<void> updateNotification(NotificationModel notification) async {
+  Future<void> updateNotification(NotificationRuleModel notification) async {
     try {
       emit(state.copyWith(isLoading: true, errorMessage: null));
+
       await _notificationRepository.updateNotification(notification);
+
+      await _notificationService.refreshScheduledNotifications();
       await loadNotifications();
     } catch (e) {
       emit(state.copyWith(errorMessage: e.toString()));
     } finally {
       if (state.isLoading) {
         emit(state.copyWith(isLoading: false));
+        log(
+          "NotificationCubit: Finished updateNotification process. Error: ${state.errorMessage}",
+        );
       }
     }
   }
 
-  Future<NotificationModel> deleteNotification(int id) async {
+  Future<NotificationRuleModel> deleteNotification(int id) async {
     try {
       emit(state.copyWith(isLoading: true, errorMessage: null));
       final notification = await _notificationRepository.getNotificationById(
@@ -98,9 +157,9 @@ class NotificationCubit extends Cubit<NotificationState> {
         );
       }
 
-      await _notificationService.cancelNotification(notification.id);
       await _notificationRepository.deleteNotification(id);
 
+      await _notificationService.refreshScheduledNotifications();
       await loadNotifications();
       return notification;
     } catch (e) {
@@ -109,6 +168,9 @@ class NotificationCubit extends Cubit<NotificationState> {
     } finally {
       if (state.isLoading) {
         emit(state.copyWith(isLoading: false));
+        log(
+          "NotificationCubit: Finished deleteNotification process. Error: ${state.errorMessage}",
+        );
       }
     }
   }
